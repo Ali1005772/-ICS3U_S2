e3e1c2f6b8262acab1cdb1fbee1301bbf49a2dd3
Ali1005772 <1005772@pdsb.net>
2025-02-10 12:18:07 -0500

    Create Unit1Lesson2.py

---
+++ Lessons/Unit1Lesson2.py
@@ -0,0 +1,20 @@
+amount = 24
+print("Two dozen is: ", amount)
+# Modify 2:
+amount = 7 * 4
+print("forty-two is: ", amount)
+# The fixed output would be
+amount = 7 * 4
+print("twenty-eight is: ", amount)
+# or
+amount = 7 * 6
+print("forty-two is: ", amount)
+# Modify 3:
+amount = 8
+amount += 1
+print("The amount is: ", amount)
+# Modify 4:
+amount = 8
+amount *= 2
+print(amount)
+# amount = amount * 2 is taking the value of amount and multiplying it by two, in this case doubling the value
